425d03d5126e676a55199fc3885c496eb24ecbdf
Christian Skaarup Enevoldsen <cskaarupe+github@egernet.dk>
2023-10-06 12:31:00 +0200

    Small upgrade to CLI

--- Sources/SequenceWS281x/SequenceWS281x.swift
+++ Sources/SequenceWS281x/SequenceWS281x.swift
@@ -29,15 +29,19 @@ struct SequenceWS281x: ParsableCommand {
         subcommands: []
     )
 
-    @Argument(help: "Executes mode: [real, app, console]")
-    var mode: String?
+    @Option(help: "Executes mode: [real, app, console]")
+    var mode: String = "app"
+
+    @Option(help: "Matrix width")
+    var matrixWidth: Int
+
+    @Option(help: "Number of leds")
+    var numberOfLeds: Int = 10
 
     func run() {
         print("\u{1B}[2J")
         print("\u{1B}[\(1);\(0)HLED will start:")
 
-        let numberOfLeds = 55 * 3
-        let matrixWidth = 3
         let sequences: [SequenceType] = [
             TestColorSequence(numberOfLeds: numberOfLeds, matrixWidth: matrixWidth),
             RainbowCycleSequence(numberOfLeds: numberOfLeds, matrixWidth: matrixWidth)
